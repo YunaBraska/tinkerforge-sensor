1fa0fd2be11c4cc8d1ac7c167631f6327c19a998
yuna90 <io@yuna.berlin>
2019-02-17 21:02:51 +0100

    fixed wrong named example

--- src/test/java/berlin/yuna/tinkerforgesensor/example/SoundIntensity_DisplaySegment_Example.java
+++ src/test/java/berlin/yuna/tinkerforgesensor/example/SoundIntensity_DisplaySegment_Example.java
@@ -7,35 +7,17 @@ import berlin.yuna.tinkerforgesensor.model.type.ValueType;
 
 public class SoundIntensity_DisplaySegment_Example {
 
-    private static long dynamicMaxVolume = 0;
-    private static SensorListener sensorListener;
     private static SensorList<Sensor> sensorList;
 
     public static void main(String[] args) {
-        sensorListener = Connection_Example.connect();
+        SensorListener sensorListener = Connection_Example.connect();
         sensorList = sensorListener.sensorList;
         sensorListener.sensorEventConsumerList.add(event -> onSensorEvent(event.value, event.valueType));
     }
 
-    public static void onSensorEvent(final Long value, final ValueType type) {
+    private static void onSensorEvent(final Long value, final ValueType type) {
         if (type.isSoundIntensity()) {
-            Sensor io16 = sensorList.getIO16();
-
-            //Dynamic max Volume
-            if (value > dynamicMaxVolume) {
-                dynamicMaxVolume = value;
-            } else {
-                dynamicMaxVolume--;
-            }
-
-            //Turn off all lights
-            io16.value(false);
-
-            //Turn on LEDs (max 16)
-            long port = ((16 * value) / dynamicMaxVolume);
-            for (int i = 0; i < port; i++) {
-                io16.value(i);
-            }
+            sensorList.getDisplaySegment().value(value / 10 + "db");
         }
     }
 }
--- src/test/java/berlin/yuna/tinkerforgesensor/example/SoundIntensity_IO16_Example.java
+++ src/test/java/berlin/yuna/tinkerforgesensor/example/SoundIntensity_IO16_Example.java
@@ -7,6 +7,7 @@ import berlin.yuna.tinkerforgesensor.model.type.ValueType;
 
 public class SoundIntensity_IO16_Example {
 
+    private static long dynamicMaxVolume = 0;
     private static SensorList<Sensor> sensorList;
 
     public static void main(String[] args) {
@@ -17,7 +18,23 @@ public class SoundIntensity_IO16_Example {
 
     private static void onSensorEvent(final Long value, final ValueType type) {
         if (type.isSoundIntensity()) {
-            sensorList.getDisplaySegment().value(value / 10 + "db");
+            Sensor io16 = sensorList.getIO16();
+
+            //Dynamic max Volume
+            if (value > dynamicMaxVolume) {
+                dynamicMaxVolume = value;
+            } else {
+                dynamicMaxVolume--;
+            }
+
+            //Turn off all lights
+            io16.value(false);
+
+            //Turn on LEDs (max 16)
+            long port = ((16 * value) / dynamicMaxVolume);
+            for (int i = 0; i < port; i++) {
+                io16.value(i);
+            }
         }
     }
 }

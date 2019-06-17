bffc2bce66d1eb2e0e1c3872701740ea6968f244
yuna90 <io@yuna.berlin>
2019-06-17 12:06:52 +0200

    servo brick added voltage change listener

--- src/main/java/berlin/yuna/tinkerforgesensor/model/sensor/brick/Servo.java
+++ src/main/java/berlin/yuna/tinkerforgesensor/model/sensor/brick/Servo.java
@@ -30,8 +30,8 @@ import static java.util.Collections.singletonList;
  * <ul>
  * <li>{@link ValueType#MOTOR_POSITION} reached position</li>
  * <li>{@link ValueType#MOTOR_VELOCITY} reached velocity</li>
- * <li>{@link ValueType#UNDER_VOLTAGE} xy[mV] voltage too low</li>
- * <li>{@link ValueType#VOLTAGE} xy[mV] output voltage change</li>
+ * <li>{@link ValueType#UNDER_VOLTAGE} [x = mV] voltage too low</li>
+ * <li>{@link ValueType#VOLTAGE} [x = mV] output voltage change</li>
  * </ul>
  * <h3>Technical Info</h3>
  * <ul>

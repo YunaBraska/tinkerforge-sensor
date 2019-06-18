7d0aa608b3ede6687a773329f82f6b735fc575db
yuna90 <io@yuna.berlin>
2019-06-18 14:11:22 +0200

    justify

--- src/test/java/berlin/yuna/hackerschool/example/Servo_Rotary_Example.java
+++ src/test/java/berlin/yuna/hackerschool/example/Servo_Rotary_Example.java
@@ -15,7 +15,6 @@ public class Servo_Rotary_Example extends Helper {
     public static void main(final String[] args) {
         stack = ConnectionAndPrintValues_Example.connect();
         stack.sensorEventConsumerList.add(event -> onSensorEvent(event.sensor, event.value, event.valueType));
-        stack.disconnect();
     }
 
     private static boolean invert = false;

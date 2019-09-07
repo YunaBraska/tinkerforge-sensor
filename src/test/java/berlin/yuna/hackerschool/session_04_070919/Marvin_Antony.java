9a9fc892f91eb4efa08c8aae771234766c0fdbaa
Yuna Morgenstern <io@yuna.berlin>
2019-09-07 14:44:00 +0200

    new template

---
+++ src/test/java/berlin/yuna/hackerschool/session_04_070919/Marvin_Antony.java
@@ -0,0 +1,34 @@
+package berlin.yuna.hackerschool.session_04_070919;
+
+import berlin.yuna.hackerschool.example.ConnectionAndPrintValues_Example;
+import berlin.yuna.hackerschool.example.Helper;
+import berlin.yuna.tinkerforgesensor.logic.Stack;
+import berlin.yuna.tinkerforgesensor.model.type.SensorEvent;
+
+
+public class Marvin_Antony extends Helper {
+
+    //VARIABLES
+    private static Stack stack;
+
+    static void onStart(){
+        loop("PoliceLED", run -> {
+            stack.sensors().buttonDual().send(-1, 2);
+        });
+    }
+
+    //CODE FUNCTION
+    static void onSensorEvent(final SensorEvent event) {
+
+
+    }
+
+    //START FUNCTION
+    public static void main(final String[] args) {
+        stack = ConnectionAndPrintValues_Example.connect();
+        stack.sensorEventConsumerList.add(Marvin_Antony::onSensorEvent);
+        onStart();
+    }
+
+
+}
